078bf050b34904652a981c16a7a08ddc10694665
Ethan Wallace <ethan.wallace.91@gmail.com>
2021-12-03 11:53:06 -0500

    Add gc-ds-button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'gc-ds-button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <gc-ds-button label="Look at me"></gc-ds-button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { applyPolyfills, defineCustomElements } from 'gc-ds-button/loader';
+import 'gc-ds-button/dist/gc-ds-button/gc-ds-button.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
